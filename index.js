810d5dedcb63e64b1c85d2d9be7f51c75b108190
Avetis <avetis.berberyan.techx@gmail.com>
2026-01-24 01:43:17 +0400

    deleted bugs of select options

--- index.js
+++ index.js
@@ -82,13 +82,6 @@ searchAll.addEventListener("change", () => {
   }
 });
 
-const selectWrapper = document.querySelector(".select-wrapper");
-const dropdownMenu = document.querySelector(".dropdown-menu");
-const selectArrow = document.querySelector(".select-arrow");
-const selectText = document.querySelector(".select-text");
-const dropdownItems = document.querySelectorAll(".dropdown-item");
-const selectedFlag = selectWrapper.querySelector(".flag-icon");
-const searchInput = document.getElementById("countrySearch");
 const checkboxItemReleasesWrapper = document.querySelector(
   ".checkbox-item-country-releases",
 );
@@ -97,80 +90,21 @@ const checkboxReleaseTypesWrapper = document.querySelector(".release-types");
 
 //releases checkboxes
 const searchAllReleases = document.getElementById("searchAllReleases");
-const searchAllCountries = document.getElementById("searchAllCountries");
-const countrySelect = document.querySelector(".country-select");
+
 searchAllReleases.addEventListener("change", () => {
   if (searchAllReleases.checked) {
     checkboxItemReleasesWrapper.style.display = "none";
     checkboxReleaseTypesWrapper.style.display = "none";
-    countrySelect.style.display = "none";
   } else {
     checkboxItemReleasesWrapper.style.display = "flex";
     checkboxReleaseTypesWrapper.style.display = "flex";
   }
 });
 
-searchAllCountries.addEventListener("change", () => {
-  if (searchAllCountries.checked) {
-    countrySelect.style.display = "none";
-  } else {
-    countrySelect.style.display = "flex";
-  }
-});
-
-// Toggle dropdown
-selectWrapper.addEventListener("click", function (e) {
-  e.stopPropagation();
-  dropdownMenu.classList.toggle("open");
-  selectArrow.classList.toggle("open");
-  if (dropdownMenu.classList.contains("open")) {
-    setTimeout(() => searchInput.focus(), 100);
-  }
-});
-
-// Prevent dropdown close when clicking search input
-searchInput.addEventListener("click", function (e) {
-  e.stopPropagation();
-});
-
-// Filter countries based on search input
-searchInput.addEventListener("input", function () {
-  const filter = this.value.toLowerCase();
-  dropdownItems.forEach((item) => {
-    const country = item.getAttribute("data-country").toLowerCase();
-    if (country.includes(filter)) {
-      item.style.display = "flex";
-    } else {
-      item.style.display = "none";
-    }
-  });
-});
-
-// Select country from dropdown
-dropdownItems.forEach((item) => {
-  item.addEventListener("click", function () {
-    const country = this.getAttribute("data-country");
-    const flagClone = this.querySelector(".flag-icon").cloneNode(true);
-
-    selectedFlag.replaceWith(flagClone);
-    selectText.textContent = country;
-
-    dropdownMenu.classList.remove("open");
-    selectArrow.classList.remove("open");
-    searchInput.value = "";
-    dropdownItems.forEach((i) => (i.style.display = "flex"));
-
-    console.log("Selected country:", country);
-  });
-});
-
-// Close dropdown when clicking outside
-document.addEventListener("click", function () {
-  dropdownMenu.classList.remove("open");
-  selectArrow.classList.remove("open");
-  searchInput.value = "";
-  dropdownItems.forEach((i) => (i.style.display = "flex"));
-});
+//country releases
+const searchAllCountries = document.getElementById("searchAllCountries");
+// TODO
+//----------------------------
 
 // date input
 
